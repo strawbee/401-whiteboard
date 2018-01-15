d986a110e00273b113e0679a0a98ce0c6b2686f6
strawbee <yuejiao.hou@gmail.com>
2018-01-15 15:17:44 -0800

    added examples

--- 01-minMaxAvg-of-array/solution.js
+++ 01-minMaxAvg-of-array/solution.js
@@ -8,3 +8,19 @@ const findArrayProperties = arr => {
     Lowest Value: ${min}
     Average of All Values: ${avg}`;
 }   
+
+// Examples
+findArrayProperties([1,2,3,4,5]);
+/*
+Highest Value: 5
+Lowest Value: 1
+Average of All Values: 3
+*/
+
+
+findArrayProperties([-18, 12, 6837, 0, 3, 10]);
+/*
+Highest Value: 6837
+Lowest Value: -18
+Average of All Values: 1140.6666666666667
+*/
