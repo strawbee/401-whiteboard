08d0903d2474d76a3555b866ecc5efdd9a557b11
strawbee <yuejiao.hou@gmail.com>
2018-01-18 09:12:11 -0800

    completed modified problem domain of returning sum

--- 03-traversing-linked-list/solution.js
+++ 03-traversing-linked-list/solution.js
@@ -3,16 +3,19 @@
 exports.traverse = obj => {
     if(!obj.value || Object.keys(obj).indexOf('next') === -1) return null;
     console.log(obj.value);
-    while (obj.next) return exports.traverse(obj.next);
+    return obj.value + (obj.next ? exports.traverse(obj.next) : 0);
 }
 
 /* Alternative, non-recursive solution:
 
 exports.traverse = obj => {
+    let sum = obj.value;
     console.log(obj.value);
     while (obj.next) {
         obj = obj.next;
+        sum += obj.value;
         console.log(obj.value);
     }
+    return sum;
 }
 */
