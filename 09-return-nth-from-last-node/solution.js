2f4e7937642d6383eca6a0175aec2398ea520c29
strawbee <yuejiao.hou@gmail.com>
2018-01-25 14:21:56 -0800

    Jan 25 2018

--- 09-return-nth-from-last-node/solution.js
+++ 09-return-nth-from-last-node/solution.js
@@ -3,7 +3,7 @@
 // O(n) - loops through copy of list twice. Once to find the list length and once to find the nth node from end.
 exports.nthFromEnd = (list, n) => {
     if (!list || typeof list !== 'object' || Array.isArray(list) || !Number.isInteger(n) || n < 0) return null;
-    let current = Object.assign({}, list), length = 0;
+    let current = list, length = 0;
     while (current) {
         if (current.value === undefined || current.next === undefined) return null;
         length++;
