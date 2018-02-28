aa3ecd79222f0a050a6ec968bada53113281efb1
strawbee <yuejiao.hou@gmail.com>
2018-02-28 13:53:45 -0800

    adding a space

--- 33-sum-digits-of-exponential/solution.js
+++ 33-sum-digits-of-exponential/solution.js
@@ -4,7 +4,7 @@ exports.sumExpDigits = (base, exp) => {
   if (!Number.isInteger(base) || !Number.isInteger(exp)) return 'Invalid argument.';
   let n = base ** exp, sum = 0;
   while (n > 0) {
-    sum+= n % 10;
+    sum += n % 10;
     n = ~~(n/10);
   }
   return sum;
