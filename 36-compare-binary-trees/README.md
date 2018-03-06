d8925bb5fede4b1ca3f707e30f1022ba9e7953fc
strawbee <yuejiao.hou@gmail.com>
2018-03-06 07:15:09 -0800

    fixing merges

--- 36-compare-binary-trees/README.md
+++
@@ -1,5 +0,0 @@
-# Whiteboard 36 - Compare BSTs
-Joy Hou, March 5, 2018
-
-## Problem Domain
-Compare two binary search trees; if they are structurally identical, return true. Otherwise, return false.
--- 36-compare-binary-trees/lib/treenode.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = class TreeNode {
-  constructor(value, left = null, right = null) {
-    this.value = value;
-    this.left = left;
-    this.right = right;
-  }
-};
